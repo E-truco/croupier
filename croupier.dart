789d231774d1fe9d8f33781eb501b0e9c3410b62
CodyKoInABox <codykoinabox@gmail.com>
2024-03-29 16:40:46 -0300

    croupier function setup and explanation

--- croupier.dart
+++ croupier.dart
@@ -13,3 +13,24 @@ class Player{
   @override
   String toString() => "ID: $id | Points: $points | Hand: $hand";
 }
+
+
+// players is a list of Player objects, should be in order of first to last to get their cards
+// cardsPerPlayer is the amount of cards each player should receive
+// deck is the deck the cards will be drawn from (following index order and removing the cards from the list as they are given)
+// cards will be given one by one, example: Player1 gets a card, then Player2 gets a card... until all players have cardsPerPlayer amount of cards
+void croupier(List<Player> players, int cardsPerPlayer, List<Card> deck){
+
+
+
+
+}
+
+
+
+
+
+void main(){
+
+
+}
